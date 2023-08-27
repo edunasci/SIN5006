23337696580c269a2553f9407a1e1b3c7ee7f7e9
Eduardo Marsola do Nascimento <52350612+edunasci@users.noreply.github.com>
2023-08-27 20:51:34 -0300

    create queens.py

---
+++ queens.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python3
+"""
+queens.py - Solve n queens problem
+"""
+__author__      = 'Eduardo Marsola do Nascimento'
+__copyright__   = 'Copyright 2023-08-27'
+__credits__     = ''
+__license__     = 'MIT'
+__version__     = '1.00'
+__maintainer__  = ''
+__email__       = ''
+__status__      = 'Production'
+__printdebug__  = False
+
+from datetime import datetime
+import random
+import json
+import itertools
+import math
+
+def createIndividual( individualSize ):
+    individual=[]
+    for i in range(1,individualSize+1):
+        individual += [i]
+        random.shuffle( individual )
+    return individual
+
+def createPopulation( populationSize, individualSize ):
+    population = []
+    for i in range(populationSize):
+        population += [createIndividual(individualSize)]
+    return population
+
+def queensMaxFitness( individualSize ):
+    return((1+individualSize)*individualSize/2)
+
+def queensFitness( individual ):
+    fitness = queensMaxFitness( len(individual) )  # starts with maximum fitness
+    for i in range(len(individual)):
+        for j in range(1,len(individual)-i):
+            print(f'i={i}, j={j}, startfitness={fitness}') if __printdebug__ else None
+            # other queen on the same line, decrease fitness by 1
+            fitness -= 1 if individual[i]==individual[i+j] else 0
+            # other queen on the same ascendent diagnoal, decrease fitness by 1
+            fitness -= 1 if individual[i]==(individual[i+j]+j) else 0
+            # other queen on the same descendent diagnoal, decrease fitness by 1
+            fitness -= 1 if individual[i]==(individual[i+j]-j) else 0
+            print(f'i={i}, j={j}, stopfitness={fitness}\n') if __printdebug__ else None
+    print(f'individual = {individual}, fitness = {fitness}')
+    return fitness
+
+    
+def queensBruteForce( individualSize ):
+    solution = []
+    firstindividual = []
+    maxFitness = queensMaxFitness( individualSize )
+    for i in range(1, individualSize+1):
+        firstindividual += [i]
+    for individual in itertools.permutations(firstindividual,r=individualSize):
+        if( queensFitness(individual) == maxFitness ):
+            solution += [individual]
+    return solution
+
+def main():
+    solutions={}
+    # solve from n=4 to n=10 by brute force
+    for n in range(4,5):
+        print(f'{datetime.now().replace(microsecond=0)}')
+        solutions[f'{n}_Queens'] = queensBruteForce(n)
+        print(f'\n\n{n} Queens Solutions ({len(solutions[f"{n}_Queens"])} best solutions):')
+        print(f'solutions[f\'{n}_Queens\'] = {solutions[f"{n}_Queens"]}')
+    
+    # write solution to a file
+    with open('QueensSolutions.csv','w') as f:
+        json.dump(solutions,f,indent=4)
+
+if __name__ == '__main__':
+    # track execution time
+    startTime=datetime.now()
+    print(f'Start: {startTime.replace(microsecond=0)}\n\n')
+    main()
+    # track execution time
+    finishTime=datetime.now()
+    print( f'\n\nStart: {startTime.replace(microsecond=0)}, Finish:{finishTime.replace(microsecond=0)}, Running Time: {finishTime-startTime}')
